ca030314677269615329f88c91808ef5532b5232
jdonohue44 <jared.donohue@gmail.com>
2017-01-06 19:41:57 -0500

    update

--- index.php
+++ index.php
@@ -194,19 +194,19 @@ function VerifyTables($connection, $dbName) {
      if(!mysqli_query($connection, $query)) echo("<p>Error creating users table.</p>");
   }
 
-  if(!TableExists("INTERESTS", $connection, $dbName))
-  {
-     $query = "CREATE TABLE `INTERESTS` (
-         `ID` int(11) NOT NULL AUTO_INCREMENT,
-         `Interest` varchar(45) DEFAULT NULL,
-         `User_Id` int(11) NOT NULL,
-         PRIMARY KEY (`ID`),
-         FOREIGN KEY (`User_Id`),
-         UNIQUE KEY `ID_UNIQUE` (`ID`)
-       ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1";
-
-     if(!mysqli_query($connection, $query)) echo("<p>Error creating interests table.</p>");
-  }
+  // if(!TableExists("INTERESTS", $connection, $dbName))
+  // {
+  //    $query = "CREATE TABLE `INTERESTS` (
+  //        `ID` int(11) NOT NULL AUTO_INCREMENT,
+  //        `Interest` varchar(45) DEFAULT NULL,
+  //        `User_Id` int(11) NOT NULL,
+  //        PRIMARY KEY (`ID`),
+  //        FOREIGN KEY (`User_Id`),
+  //        UNIQUE KEY `ID_UNIQUE` (`ID`)
+  //      ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1";
+  //
+  //    if(!mysqli_query($connection, $query)) echo("<p>Error creating interests table.</p>");
+  // }
 }
 
 
