76c11cda30e33e9d70e343f57ba2b549bab5ec9a
jdonohue44 <jared.donohue@gmail.com>
2017-01-08 16:19:31 -0500

    update

--- updates.php
+++
@@ -1,6 +0,0 @@
-<?php
-function updateInterestsTable(){
-  $query = "INSERT INTO `USERS` (`Name`,`Email`) VALUES ('$n', '$e');";
-  if(!mysqli_query($connection, $query)) echo("<p>Error adding employee data.</p>");
-}
-?>
