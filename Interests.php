b3428b35bf455b07aa7de418189f9599f9f5b4c4
jdonohue44 <jared.donohue@gmail.com>
2017-01-08 13:34:59 -0500

    update

--- Interests.php
+++ Interests.php
@@ -15,7 +15,7 @@ session_start();
     $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
     if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
     $database = mysqli_select_db($connection, DB_DATABASE);
-    print_r($_POST['Interest']);
+    print_r($_POST);
     //updateInterestsTable($name, $email);
     ?>
   <h2>Interests</h2>
