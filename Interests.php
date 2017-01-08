c8f0f7e73731dea2e3873005c5433542020b9968
jdonohue44 <jared.donohue@gmail.com>
2017-01-08 17:58:15 -0500

    update

--- Interests.php
+++ Interests.php
@@ -21,7 +21,9 @@ session_start();
     $user_interests = $_POST['interests'];
     if(sizeof($user_interests)>0){
       for($i = 0; $i < sizeof($user_interests); $i++){
-          echo "Interest: " . $user_interests[$i];
+          echo "Interest: " . $user_interests[$i] .
+           "size of array: " . sizeof($user_interests) ."\n";
+
           AddInterest($connection, $user_interests[$i]);
           AddUserInterest($connection, $email, $user_interests[$i]);
       }
