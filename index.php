6eb16bcb309a67142bfa88b0aadef78a6282809b
jdonohue44 <jared.donohue@gmail.com>
2017-01-10 10:52:28 -0500

    update

--- index.php
+++ index.php
@@ -40,8 +40,8 @@ session_start();
 <div class="container">
 <!-- Input form -->
 <form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
-        <input type="text" name="Name"  id="name_text" placeholder="Name" tabindex=1 maxlength="25" size="30" />
-        <input type="text" name="Email" id="email_text" placeholder="Email" tabindex=2 maxlength="25" size="40" />
+        <input type="text" name="Name"  id="name_text" placeholder="Name" tabindex=1 maxlength="30" size="30" />
+        <input type="text" name="Email" id="email_text" placeholder="Email" tabindex=2 maxlength="35" size="40" />
         <h5 id="validation_typing"></h5>
         <input type="submit" name="validate_button" value="Lets Go!" />
 </form>
